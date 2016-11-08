91b82f90990b31e3e329877c8415e0829914f1af
Tomasz Wylężek <tomasz.wylezek@jcommerce.pl>
2016-11-08 13:12:09 +0100

    routing complete

--- APM - Start/app/products/product-detail.component.ts
+++ APM - Start/app/products/product-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { IProduct } from './product';
 @Component({
-    templateUrl: 'product-detail.component.html'
+    templateUrl: 'app/products/product-detail.component.html'
 })
 
 export class ProductDetailComponent {
--- APM - Start/app/products/product-list.component.ts
+++ APM - Start/app/products/product-list.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit }  from '@angular/core';
+
 import { IProduct } from './product';
 import { ProductService } from './product.service';
 
 @Component({
-    selector: 'pm-products',
-    moduleId: module.id,
-    templateUrl: 'product-list.component.html',
-    styleUrls: ['product-list.component.css']
+    templateUrl: 'app/products/product-list.component.html',
+    styleUrls: ['app/products/product-list.component.css']
 })
 
 export class ProductListComponent implements OnInit {
